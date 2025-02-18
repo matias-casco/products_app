7b871ea63ed8e6b7f1759ed12761a8f5a7dd6fc7
Matias Casco <kon-dcasco@bancobasa.com.py>
2025-02-18 18:59:57 -0300

    feat: add basic content to product details page

--- lib/products/presentation/pages/product_details.dart
+++ lib/products/presentation/pages/product_details.dart
@@ -8,15 +8,33 @@ class ProductDetailsExtra {
 }
 
 class ProductDetailsPage extends StatelessWidget {
-  const ProductDetailsPage({super.key, this.extra});
+  const ProductDetailsPage({super.key, required this.extra});
 
   static const name = 'ProductDetails';
   static const path = '/products_details';
 
-  final ProductDetailsExtra? extra;
+  final ProductDetailsExtra extra;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Product Details')),
+      body: Column(
+        children: [
+          Image.network(
+            extra.details.thumbnail,
+            width: double.infinity,
+            height: 180,
+            fit: BoxFit.scaleDown,
+          ),
+          Text('Product Name: ${extra.details.title}'),
+          Text('Product Price: ${extra.details.price}'),
+          Text('Product Category: ${extra.details.category}'),
+          Text('Product Rating: ${extra.details.rating}'),
+          Text('Product Description: ${extra.details.description}'),
+          
+        ],
+      ),
+    );
   }
 }
