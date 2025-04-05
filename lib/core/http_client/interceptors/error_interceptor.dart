ddbe88a8c1f1c3ac3283662bac3691613f3c8fc6
Matias Casco <kon-dcasco@bancobasa.com.py>
2025-04-05 13:21:42 -0300

    feat: add crashlytics to error interceptor

--- lib/core/http_client/interceptors/error_interceptor.dart
+++ lib/core/http_client/interceptors/error_interceptor.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'package:products_app/core/http_client/exceptions/exceptions.dart';
 
@@ -9,6 +11,16 @@ class ErrorInterceptor extends Interceptor {
     ErrorInterceptorHandler handler,
   ) async {
     if (err.response == null) {
+      FirebaseCrashlytics.instance.recordError(
+        err,
+        StackTrace.current,
+        reason: [
+          'Error: ${err.message}',
+          'Request: ${err.requestOptions.method} ${err.requestOptions.path}',
+          'Response: ${err.response?.statusCode} ${err.response?.statusMessage}',
+        ],
+      );
+
       throw DefaultException(
         code: GenericException.getErrorCodeFromString('GENERIC_ERROR'),
         returnMessage: 'Status code ${err.response?.statusCode} ${err.message}',
@@ -20,6 +32,16 @@ class ErrorInterceptor extends Interceptor {
     if ((err.response!.statusCode ?? 400) > 399 &&
         (err.response!.statusCode ?? 0) < 500) {
       if (err.response!.data is Map<String, dynamic>) {
+        FirebaseCrashlytics.instance.recordError(
+          err,
+          StackTrace.current,
+          reason: [
+            'Error: ${err.message}',
+            'Request: ${err.requestOptions.method} ${err.requestOptions.path}',
+            'Response: ${err.response?.statusCode} ${err.response?.statusMessage}',
+          ],
+        );
+
         if (err.response!.statusCode == 400) {
           throw BadRequestException(
             code: GenericException.getErrorCodeFromString('BAD_REQUEST'),
@@ -42,6 +64,15 @@ class ErrorInterceptor extends Interceptor {
 
     if (err.response!.statusCode! >= 500) {
       if (err.response!.statusCode == 503) {
+        FirebaseCrashlytics.instance.recordError(
+          err,
+          StackTrace.current,
+          reason: [
+            'Error: ${err.message}',
+            'Request: ${err.requestOptions.method} ${err.requestOptions.path}',
+            'Response: ${err.response?.statusCode} ${err.response?.statusMessage}',
+          ],
+        );
         throw DefaultException(
           code: GenericException.getErrorCodeFromString('GENERIC_ERROR'),
           returnMessage:
